fefe047f3a19a689fc95437dc9a59b4375ceec38
Thomas <tlloevkilde@gmail.com>
2024-02-23 12:03:37 +0100

    Added CalculationLog, Utilities and DbContext

---
+++ AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DockerCalculator;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<CalculationLog> CalculationLogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            Utilities.GetConnectionString(), 
+            ServerVersion.AutoDetect(Utilities.GetConnectionString())
+            );
+    }
+}
+
---
+++ CalculationLog.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DockerCalculator;
+
+public class CalculationLog
+{   
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    public int Number1 { get; set; }
+    [Required]
+    public int Number2 { get; set; }
+    [Required]
+    public string Operation { get; set; }
+    [Required]
+    public int Result { get; set; }
+}
