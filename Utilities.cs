80b71c4834b72be226657cba02dac1e7c11f3d6d
Thomas <tlloevkilde@gmail.com>
2024-02-25 13:36:16 +0100

    Stuck with compose

--- Utilities.cs
+++ Utilities.cs
@@ -4,20 +4,31 @@ public class Utilities
 {
     public static string GetConnectionString()
         {
+            // var database = "DockerCalculator";
+            // var server = Environment.GetEnvironmentVariable("DockerDbServer") ?? "mysql-server";
+            // var port = Environment.GetEnvironmentVariable("DockerDbPort") ?? "3306";
+            // var user = Environment.GetEnvironmentVariable("DockerDbUser") ?? "root";
+            // var password = Environment.GetEnvironmentVariable("DockerDbPassword") ?? "1234";
+
             var database = "DockerCalculator";
-            var server = Environment.GetEnvironmentVariable("db_server");
-            var user = Environment.GetEnvironmentVariable("db_user");
-            var password = Environment.GetEnvironmentVariable("db_passwd");
+            var server = "mysql-server";
+            var port = "3307";
+            var user = "root";
+            var password = "1234";
            
             // Check if any of the required environment variables are missing
-            if (string.IsNullOrEmpty(server) || string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(database))
+            if (string.IsNullOrEmpty(database) 
+                || string.IsNullOrEmpty(server)
+                || string.IsNullOrEmpty(port) 
+                || string.IsNullOrEmpty(user) 
+                || string.IsNullOrEmpty(password))
             {
                 throw new InvalidOperationException("Missing database environment variables.");
             }
 
 
             // Construct and return the connection string
-            return $"server={server};user={user};password={password};database={database};";
+            return $"database={database}; server={server}; port={port}; user={user}; password={password};";
         }
 }
 
