9f24435d03289006392cb5f7527415edaf7666e7
Thomas <tlloevkilde@gmail.com>
2024-02-28 15:49:33 +0100

    fixed dotnet ef. Problem with mysql root password empty

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
-FROM mcr.microsoft.com/dotnet/sdk:8.0 
+FROM mcr.microsoft.com/dotnet/sdk:8.0
+
 COPY . /app
 WORKDIR /app
 
-CMD ["dotnet", "ef", "database", "update", "&&", "dotnet", "run"]
+RUN dotnet restore
+RUN dotnet tool install --global dotnet-ef
+
+ENV PATH="${PATH}:/root/.dotnet/tools"
+
+CMD ["sh", "-c", "dotnet ef database update && dotnet run"]
+
+
 
 
--- Utilities.cs
+++ Utilities.cs
@@ -3,32 +3,32 @@
 public class Utilities
 {
     public static string GetConnectionString()
+    {
+        // var database = "DockerCalculator";
+        // var server = Environment.GetEnvironmentVariable("DockerDbServer") ?? "mysql-server";
+        // var port = Environment.GetEnvironmentVariable("DockerDbPort") ?? "3306";
+        // var user = Environment.GetEnvironmentVariable("DockerDbUser") ?? "root";
+        // var password = Environment.GetEnvironmentVariable("DockerDbPassword") ?? "1234";
+
+        var database = "DockerCalculator";
+        var server = "mysql-server";
+        var port = "3307";
+        var user = "root";
+        var password = "1234";
+
+        // Check if any of the required environment variables are missing
+        if (string.IsNullOrEmpty(database)
+            || string.IsNullOrEmpty(server)
+            || string.IsNullOrEmpty(port)
+            || string.IsNullOrEmpty(user)
+            || string.IsNullOrEmpty(password))
         {
-            // var database = "DockerCalculator";
-            // var server = Environment.GetEnvironmentVariable("DockerDbServer") ?? "mysql-server";
-            // var port = Environment.GetEnvironmentVariable("DockerDbPort") ?? "3306";
-            // var user = Environment.GetEnvironmentVariable("DockerDbUser") ?? "root";
-            // var password = Environment.GetEnvironmentVariable("DockerDbPassword") ?? "1234";
-
-            var database = "DockerCalculator";
-            var server = "mysql-server";
-            var port = "3307";
-            var user = "root";
-            var password = "1234";
-           
-            // Check if any of the required environment variables are missing
-            if (string.IsNullOrEmpty(database) 
-                || string.IsNullOrEmpty(server)
-                || string.IsNullOrEmpty(port) 
-                || string.IsNullOrEmpty(user) 
-                || string.IsNullOrEmpty(password))
-            {
-                throw new InvalidOperationException("Missing database environment variables.");
-            }
-
-
-            // Construct and return the connection string
-            return $"database={database}; server={server}; port={port}; user={user}; password={password};";
+            throw new InvalidOperationException("Missing database environment variables.");
         }
+
+
+        // Construct and return the connection string
+        return $"database={database}; server={server}; port={port}; user={user}; password={password};";
+    }
 }
 
